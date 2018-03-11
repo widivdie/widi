6a27d9d402fa53dd495fa121b51c31930c702d54
widivdie <37254780+widivdie@users.noreply.github.com>
2018-03-11 22:18:31 +0700

    Update panggil.js

--- panggil.js
+++ panggil.js
@@ -22,8 +22,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}
-if(x=="2"){xxx="/2017/08/tesla-car-battery-production-releases.html"}a_to_ve[i].href="https://downloadappmod.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/03/free-download-metal-slug-attack-v310.html"}
+if(x=="2"){xxx="/2018/03/free-download-kim-kardashian-hollywood.html"}a_to_ve[i].href="https://downloadappmod.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
